4892ddb2138ebcae6b8b3980c868570f88dc163e
Ori Sela <orisela3@gmail.com>
2024-05-13 03:06:27 +0300

    tweak api

--- src/api.ts
+++ src/api.ts
@@ -5,7 +5,7 @@ const apiPath = 'http://localhost:3000/api';
 export const getApi = (resource: string) => axios(`${apiPath}/${resource}`);
 
 export const postApi = (resource: string, data?: any) =>
-  axios.post(`${apiPath}${resource}`, data);
+  axios.post(`${apiPath}/${resource}`, data);
 
 export const patchApi = (resource: string, data: any) =>
-  axios.patch(`${apiPath}${resource}`, data);
+  axios.patch(`${apiPath}/${resource}`, data);
