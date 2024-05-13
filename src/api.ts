12d004717eaf68b6229268cb2b7265e1856fbb0c
Ori Sela <orisela3@gmail.com>
2024-05-13 03:04:44 +0300

    create api util

---
+++ src/api.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiPath = 'http://localhost:3000/api';
+
+export const getApi = (resource: string) => axios(`${apiPath}/${resource}`);
+
+export const postApi = (resource: string, data?: any) =>
+  axios.post(`${apiPath}${resource}`, data);
+
+export const patchApi = (resource: string, data: any) =>
+  axios.patch(`${apiPath}${resource}`, data);
